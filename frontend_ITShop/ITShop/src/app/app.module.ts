9ab56acdc8885a3d01e489522ea3b1220831a5cf
Đani Husković <djani.huskovic@edu.fit.ba>
2023-01-21 17:37:25 +0100

    Add navbar, shop, not-found and category components and routes

--- frontend_ITShop/ITShop/src/app/app.module.ts
+++ frontend_ITShop/ITShop/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShopComponent } from './shop/shop.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CategoryComponent } from './category/category.component';
+
+const routes: Routes = [
+  {path: 'categories', component: CategoryComponent},
+  {path: '', pathMatch: 'full', component: ShopComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    ShopComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
   providers: [],
+  exports: [ RouterModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
